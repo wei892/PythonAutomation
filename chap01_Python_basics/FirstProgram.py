5460353a3461c6f4062caf357a25981fd2b4deb8
Wei <wlin.li2403@gmail.com>
2024-06-17 15:02:00 -0400

    reorganized folders

---
+++ chap01_Python_basics/FirstProgram.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+print("What is your name?")
+
+name = input()
+print("It is good to meet you " + name)
+print("The length of your name is: ")
+print(len(name))
+
+print("What is your age?")
+age = input()
+print("You will be " + str(int(age) + 1) + " in a year.")
